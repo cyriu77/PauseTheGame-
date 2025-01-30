25f59cdeb2a2c06fb471474c9b131cc5e9e8e6f6
cyriu77 <lagmon.1981@mail.ru>
2025-01-31 04:04:48 +0500

    Create PausePanelGame.cs

---
+++ PausePanelGame.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PausePanelGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool isPauPanPased = false;
+    public GameObject pauseShPanel;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPauPanPased)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+       
+    }
+    public void Pause()
+    {
+        pauseShPanel.SetActive(true);
+        Time.timeScale = 0f;
+
+        isPauPanPased = true;
+    }
+    public void Resume()
+    {
+
+        pauseShPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPauPanPased = false;
+    }
+}
